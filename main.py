34950c751f455ff8eb1f4b87c0f33e456a15ca0d
jcjoiner <jcjoiner555@gmail.com>
2024-02-24 12:56:34 -0600

    Learned how to spell consonants.

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 # Problem Set 3
-# Vowels and Constonants
+# Vowels and Consonants
 
 '''
     Write a program that takes a string input and counts the number of vowels and consonants in it.
 '''
 
 vowels = 0
-constonants = 0
+consonants = 0
 
 usr_string = input("Enter your string: ")
 for letter in usr_string:
     if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
         vowels += 1
     else:
-        constonants += 1
+        consonants += 1
 
 print(f"There were {vowels} vowels.")
-print(f"There were {constonants} constonants.")
+print(f"There were {consonants} consonants.")
